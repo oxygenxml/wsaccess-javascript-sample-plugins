13cf561e8ef88ed0e82b83af2193aa101755ee04
Radu Coravu <radu_coravu@sync.ro>
2016-11-22 14:43:46 +0200

    Allow filtering messages both by error key and message string.

--- validationProblemsFilter/wsAccess.js
+++ validationProblemsFilter/wsAccess.js
@@ -1,19 +1,36 @@
 function applicationStarted(pluginWorkspaceAccess) {
- Packages.java.lang.System.err.println("Application started " + pluginWorkspaceAccess);
+ messageContentsList = readConfigFileContents(pluginWorkspaceAccess, "messageContentsToFilter.txt");
+ keysToFilter = readConfigFileContents(pluginWorkspaceAccess, "keysToFilter.txt");
  edChangedListener = {
   /*Called when an editor  is opened*/
   editorOpened: function (editorLocation) {
-   Packages.java.lang.System.err.println("\nrunning " + editorLocation);
    /*Get the opened editor*/
    editor = pluginWorkspaceAccess.getEditorAccess(editorLocation, Packages.ro.sync.exml.workspace.api.PluginWorkspace.MAIN_EDITING_AREA);
    /*Add validation problems filter*/
    validationProblemsFilter = {
     filterValidationProblems: function (validationProblems) {
-     Packages.java.lang.System.err.println("VALIDATE");
      problemsList = validationProblems.getProblemsList();
       if(problemsList != null){
         for (i = problemsList.size() - 1; i >=0; i--) {
-        if("dmv.reference.not.exist" == problemsList.get(i).getErrorKey()){
+        problem = problemsList.get(i);
+        remove = false;
+        //Filter message fragments
+        for (k = 0; k < messageContentsList.size(); k++) {
+          if(problem.getMessage() != null && problem.getMessage().contains(messageContentsList.get(k))){
+              remove = true;
+              break;
+          }
+        }
+        //Filter error keys.
+        if(! remove){
+          for (k = 0; k < keysToFilter.size(); k++) {
+          if(problem.getErrorKey() != null && problem.getErrorKey().equals(keysToFilter.get(k))){
+              remove = true;
+              break;
+          }
+         }  
+        }
+        if(remove){
           problemsList.remove(i);
         }
        }
@@ -30,5 +47,17 @@ function applicationStarted(pluginWorkspaceAccess) {
 }
 
 function applicationClosing(pluginWorkspaceAccess) {
- Packages.java.lang.System.err.println("Application closing " + pluginWorkspaceAccess);
+ //
+}
+
+//Read contents of the UTF-8 encoded config file, line by line to a list.
+function readConfigFileContents(pluginWorkspaceAccess, fileName){
+     messageContentsToFilterOutURL = pluginWorkspaceAccess.getUtilAccess().locateFile(jsDirURL) + "/" + fileName;
+     reader = new Packages.java.io.BufferedReader(new Packages.java.io.InputStreamReader(new Packages.java.io.FileInputStream(messageContentsToFilterOutURL), "UTF8"));
+     list = new Packages.java.util.ArrayList();
+    while ((line = reader.readLine()) != null) {
+        list.add(line);
+    }
+    reader.close();
+    return list;
 }
