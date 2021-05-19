04b369109610615d7752af53d133ea82d2eaa0b0
Radu Coravu <radu_coravu@sync.ro>
2021-05-19 06:55:55 +0300

    Example of using API to impose an options key

---
+++ impose-option-keys/wsAccess.js
@@ -0,0 +1,5 @@
+function applicationStarted(pluginWorkspaceAccess) {
+ pluginWorkspaceAccess.setGlobalObjectProperty("recently.used.characters", "δΔ");
+}
+function applicationClosing(pluginWorkspaceAccess) {
+}
